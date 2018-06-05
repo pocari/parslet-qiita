40e9d555e13b7c7721760c6a330fb2dda29af0c2
pocari <caffelattenonsugar@gmail.com>
2018-06-05 23:21:03 +0900

    BinOpNodeの変換も追加#

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ class CalcParser < Parslet::Parser
     (sign.maybe >> integer >> decimal.maybe).as(:number) >> space?
   }
 
-  rule(:term) { number.as(:left) >> (term_op >> number.as(:right)).repeat }
+  rule(:term) { (number.as(:left) >> (term_op >> number.as(:right)).repeat).as(:term) }
   rule(:term_op) { match('[+-]').as(:op) >> space? }
 
   rule(:space) { match('\s').repeat(1) }
@@ -22,15 +22,32 @@ class CalcParser < Parslet::Parser
   root(:term)
 end
 
+# 二項演算用クラス
+BinOpNode = Struct.new(:left, :op, :right)
+
 class CalcTransform < Parslet::Transform
   rule(number: simple(:x)) { x.to_f }
   # left: xxxの構造も xxxに変換する
   rule(left: simple(:x)) { x }
+  rule(term: subtree(:tree)) {
+    if tree.is_a?(Array)
+      # 配列ならBinOpNodeに変換
+      tree.inject do |left, op_right|
+        # 演算子と右辺を取り出し
+        op = op_right[:op]
+        right = op_right[:right]
+        BinOpNode.new(left, op, right)
+      end
+    else
+      # 配列でないならそのものを返す
+      tree
+    end
+  }
 end
 
 parsed = CalcParser.new.parse('1 + 2 - 3')
 p parsed
-# => [{:left=>{:number=>"1"@0}}, {:op=>"+"@2, :right=>{:number=>"2"@4}}, {:op=>"-"@6, :right=>{:number=>"3"@8}}]
+# => {:term=>[{:left=>{:number=>"1"@0}}, {:op=>"+"@2, :right=>{:number=>"2"@4}}, {:op=>"-"@6, :right=>{:number=>"3"@8}}]}
 
 p CalcTransform.new.apply(parsed)
-# => [1.0, {:op=>"+"@2, :right=>2.0}, {:op=>"-"@6, :right=>3.0}]
+# => #<struct BinOpNode left=#<struct BinOpNode left=1.0, op="+"@2, right=2.0>, op="-"@6, right=3.0>
