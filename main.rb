b6b202a8fd5fa607e14409a9865d84374f45a744
pocari <caffelattenonsugar@gmail.com>
2018-06-05 21:41:12 +0900

    空白考慮なしのas

--- main.rb
+++ main.rb
@@ -12,7 +12,8 @@ class CalcParser < Parslet::Parser
   rule(:number) {
     sign.maybe >> integer >> decimal.maybe >> space?
   }
-  rule(:term) { number >> (term_op >> number).repeat }
+
+  rule(:term) { number.as(:left) >> (term_op.as(:op) >> number.as(:right)).repeat }
   rule(:term_op) { match('[+-]') >> space? }
 
   rule(:space) { match('\s').repeat(1) }
@@ -21,8 +22,5 @@ class CalcParser < Parslet::Parser
   root(:term)
 end
 
-p CalcParser.new.parse('1+2-3')
-# => "1+2-3"@0
-p CalcParser.new.parse('1+2-')
-# => Parslet::ParseFailed -の項が足りないのでエラーが出る
-#
+p CalcParser.new.parse('1 + 2 - 3')
+# => [{:left=>"1 "@0}, {:op=>"+ "@2, :right=>"2 "@4}, {:op=>"- "@6, :right=>"3"@8}]
