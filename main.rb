e9f3756fc1fce50d37d7b7ee9b6fd69b219fec28
pocari <caffelattenonsugar@gmail.com>
2018-06-05 22:46:43 +0900

    leftの変換ルールも追加

--- main.rb
+++ main.rb
@@ -10,13 +10,10 @@ class CalcParser < Parslet::Parser
     str('.') >> match('[0-9]').repeat
   }
   rule(:number) {
-    # 数値部分に対して :number でキャプチャする
     (sign.maybe >> integer >> decimal.maybe).as(:number) >> space?
   }
 
-  # 数値や演算子は各レベルでキャプチャしているので、ここでは左辺、右辺のキャプチャのみ
   rule(:term) { number.as(:left) >> (term_op >> number.as(:right)).repeat }
-  # 演算子に対して :op でキャプチャする
   rule(:term_op) { match('[+-]').as(:op) >> space? }
 
   rule(:space) { match('\s').repeat(1) }
@@ -25,18 +22,15 @@ class CalcParser < Parslet::Parser
   root(:term)
 end
 
-# 追加したTransformクラス
 class CalcTransform < Parslet::Transform
   rule(number: simple(:x)) { x.to_f }
+  # left: xxxの構造も xxxに変換する
+  rule(left: simple(:x)) { x }
 end
 
-# まずパースする
 parsed = CalcParser.new.parse('1 + 2 - 3')
-# パース結果表示
 p parsed
 # => [{:left=>{:number=>"1"@0}}, {:op=>"+"@2, :right=>{:number=>"2"@4}}, {:op=>"-"@6, :right=>{:number=>"3"@8}}]
 
-# パース結果をtransformした結果を表示
 p CalcTransform.new.apply(parsed)
-# => [{:left=>1.0}, {:op=>"+"@2, :right=>2.0}, {:op=>"-"@6, :right=>3.0}]
-
+# => [1.0, {:op=>"+"@2, :right=>2.0}, {:op=>"-"@6, :right=>3.0}]
