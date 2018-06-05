6f6d9b329ccefa8316aec8a1ead74bf964572498
pocari <caffelattenonsugar@gmail.com>
2018-06-05 21:35:55 +0900

    asなしの加減算ルール追加

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'parslet'
 
-class NumParser < Parslet::Parser
+class CalcParser < Parslet::Parser
   rule(:sign) { match('[-+]') }
   rule(:integer) {
     (match('[1-9]') >> match('[0-9]').repeat) |
@@ -10,12 +10,19 @@ class NumParser < Parslet::Parser
     str('.') >> match('[0-9]').repeat
   }
   rule(:number) {
-    sign.maybe >> integer >> decimal.maybe
+    sign.maybe >> integer >> decimal.maybe >> space?
   }
+  rule(:term) { number >> (term_op >> number).repeat }
+  rule(:term_op) { match('[+-]') >> space? }
 
   rule(:space) { match('\s').repeat(1) }
   rule(:space?) { space.maybe }
 
-  root(:number)
+  root(:term)
 end
 
+p CalcParser.new.parse('1+2-3')
+# => "1+2-3"@0
+p CalcParser.new.parse('1+2-')
+# => Parslet::ParseFailed -の項が足りないのでエラーが出る
+#
