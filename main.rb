7940596152b25c58bdce05dfc625b375bced4257
pocari <caffelattenonsugar@gmail.com>
2018-06-05 15:52:05 +0900

    integerルール作成

--- main.rb
+++ main.rb
@@ -2,8 +2,15 @@ require 'parslet'
 
 class NumParser < Parslet::Parser
   rule(:sign) { match('[-+]').maybe }
+  rule(:integer) {
+    (match('[1-9]') >> match('[0-9]').repeat) |
+    match('[0-9]')
+  }
 end
 
-p NumParser.new.sign.parse('-')
-p NumParser.new.sign.parse('+')
-p NumParser.new.sign.parse('')
+p NumParser.new.integer.parse('0')
+# => "0"@@
+p NumParser.new.integer.parse('10')
+# => "10"@@
+p NumParser.new.integer.parse('01')
+# => Parslet::ParseFailed パース失敗
